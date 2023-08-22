6f9071c33853078bced3b5098d3c018282ebf1a7
ChyngyzTorogeldiev <77287466+ChyngyzTorogeldiev@users.noreply.github.com>
2023-08-22 07:30:43 +0600

    add 555

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, request
 
 @app.route('/')
 def homepage():
 	f = open('goods.txt', 'r+', encoding='utf-8')
 	txt = f.readlines()
 	return render_template('index.html', goods=txt)
+
+@app.route('/add/', methods=["POST"])
+def add():
+	good = request.form["good"]
+	f = open('goods.txt', 'a+', encoding='utf-8')
+	f.write(good + "\n")
+	f.close()
+	return "Инвентарь пополнен"
